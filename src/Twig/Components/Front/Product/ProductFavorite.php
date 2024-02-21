d4de920920409e180c446f6c3a7a8c923a4d0967
Rauum <vache.romain30@gmail.com>
2024-02-21 08:40:43 +0100

    Mise en place du sytème d'ajout au favoris via live component (lets go enfin)

--- src/Twig/Components/Front/Product/ProductFavorite.php
+++ src/Twig/Components/Front/Product/ProductFavorite.php
@@ -1,32 +1,75 @@
 <?php
-namespace App\Components;
+namespace App\Twig\Components\Front\Product;
 
 use App\Entity\Favorite;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\FavoriteRepository;
+use App\Repository\ProductRepository;
+use App\Repository\UserRepository;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
-use Symfony\UX\LiveComponent\DefaultActionTrait;
-use Symfony\UX\LiveComponent\ValidatableComponentTrait;
 
 #[AsLiveComponent]
 class ProductFavorite
 {
     use DefaultActionTrait;
 
-    #[LiveProp()]
-    public Favorite $favorite;
+    #[LiveProp(writable: true)]
+    public int $user_id;
 
-    public bool $isSaved = false;
+    #[LiveProp(writable: true)]
+    public int $product_id;
+
+    public bool $isFav;
+
+    public function __construct(
+        private FavoriteRepository $favoriteRepository,
+        private ProductRepository $productRepository,
+        private UserRepository $userRepository
+    ) {
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function isFav(): bool
+    {
+        $favorite = $this->favoriteRepository->findOneBy(['user'=>$this->user_id,'product'=>$this->product_id]);
+        if($favorite != null){
+            $this->isFav = true;
+        }
+        else {
+            $this->isFav = false;
+        }
+        return $this->isFav;
+    }
 
     #[LiveAction]
-    public function save(EntityManagerInterface $entityManager)
+    public function add()
     {
-        //$this->validate();
+        $favorite = new Favorite();
+        $favorite->setUser($this->userRepository->find($this->user_id));
+        $favorite->setProduct($this->productRepository->find($this->product_id));
+        $favorite->setCreatedAt(new \DateTimeImmutable());
+        $this->favoriteRepository->add($favorite);
+        $this->isFav = true;
+    }
 
-        $this->isSaved = true;
+    #[LiveAction]
+    public function delete()
+    {
+        $favorite = $this->favoriteRepository->findOneBy(['user'=>$this->user_id,'product'=>$this->product_id]);
+        if($favorite != null){
+            $this->favoriteRepository->remove($favorite);
+        }
+        $this->isFav = false;
+    }
 
-        $entityManager->flush();
+    public function getCountFavs(): int
+    {
+        $count = count($this->favoriteRepository->findBy(['product'=>$this->product_id]));
+        return $count;
     }
 }
