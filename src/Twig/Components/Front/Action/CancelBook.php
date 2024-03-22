d93bdf76337cc4e5d016dbf29efc6f91426110fa
Romain Vaché <vache.romain30@gmail.com>
2024-03-22 17:13:49 +0100

    Mise en place des actions sous forme de components

---
+++ src/Twig/Components/Front/Action/CancelBook.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Twig\Components\Front\Action;
+
+use App\Entity\Sport;
+use App\Enum;
+use App\Repository\SportRepository;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+class CancelBook
+{
+    public string $uuid;
+    public string $class;
+    public string $custom_class = "";
+}
